fb79765bf5fc5122c7b8415accc2b3f8c6505e4e
Rajit <73546886+rajitkuthialamuj@users.noreply.github.com>
2021-08-31 17:39:17 +0530

    Intialized plus header
    Initialized the project and added some react-bootstrap element that is the header on the home page

---
+++ habitup/src/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import * as ReactBootstrap from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SiWolfram } from 'react-icons/si';
+
+const Header=()=>{
+  return(  <ReactBootstrap.Navbar bg="light" expand="lg">
+  <ReactBootstrap.Container>
+    <ReactBootstrap.Navbar.Brand href="#home"><SiWolfram /> <strong>HabitUp</strong></ReactBootstrap.Navbar.Brand>
+    <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+      <ReactBootstrap.Nav className="me-auto">
+        <ReactBootstrap.Nav.Link href="/home">Home</ReactBootstrap.Nav.Link>
+        <ReactBootstrap.Nav.Link href="/todo-list">Todo List</ReactBootstrap.Nav.Link>
+
+      </ReactBootstrap.Nav>
+    </ReactBootstrap.Navbar.Collapse>
+  </ReactBootstrap.Container>
+</ReactBootstrap.Navbar> );
+}
+
+
+
+export default Header;
