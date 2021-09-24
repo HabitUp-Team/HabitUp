783fe5706e80a13316372400e0b5d07dc1b30dbd
Rajit <73546886+rajitkuthialamuj@users.noreply.github.com>
2021-09-24 10:19:33 +0530

    firebase fixed

--- habitup/src/FirebaseConfig.js
+++ habitup/src/FirebaseConfig.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "habitup-ff2ab.firebaseapp.com",
-    projectId: "habitup-ff2ab",
-    storageBucket: "habitup-ff2ab.appspot.com",
-    messagingSenderId: "671113585638",
-    appId: "1:671113585638:web:9d61ce3a816ffaa45bac27",
-    measurementId: "G-MCZ2X8MTES"
+    authDomain: "habitup-f5bb5.firebaseapp.com",
+  projectId: "habitup-f5bb5",
+  storageBucket: "habitup-f5bb5.appspot.com",
+  messagingSenderId: "218033068607",
+  appId: "1:218033068607:web:c0f180a7860ad9c28eb68a",
+  measurementId: "G-9TZ2295EJB"
 };
 
 firebase.initializeApp(firebaseConfig);
