3b6e1655cd854dc23edeffe663c04e712fc0fe5b
arnavk09 <arnav02@outlook.com>
2021-09-28 19:40:06 +0530

    added basic tasker functionality #1

--- habitup/src/components/todo.jsx
+++ habitup/src/components/todo.jsx
@@ -1,29 +1,28 @@
-import React, { useState ,useEffect} from "react";
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { PaddingStyles } from "../styles/PaddingStyles";
 import { H3 } from "../styles/heading-three";
 import TodoItem from "./todo-item";
 import { Username } from "../AuthData";
-import { Button, FormControl } from 'react-bootstrap';
-import InputGroup from 'react-bootstrap/InputGroup'
+import { Button, FormControl } from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
 import firebase from "../FirebaseConfig";
-import {gettingTodoList,settingTodoList} from "../Database";
+import { gettingTodoList, settingTodoList } from "../Database";
 
 const Todo = () => {
-
   const [Records, setRecords] = useState([]);
   const [todoItem, setodoItem] = useState({
     todoTask: "",
     impactSign: "",
-    id: uuidv4()
+    id: uuidv4(),
   });
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setodoItem({ ...todoItem, [name]: value });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -32,21 +31,22 @@ const Todo = () => {
     setodoItem({
       todoTask: "",
       impactSign: "",
-      id: ""
+      id: "",
     });
-
-  }
+  };
   useEffect(() => {
-    gettingTodoList().then((e) => setRecords(e))
+    gettingTodoList().then((e) => setRecords(e));
   }, []);
-   
-const ref = firebase.firestore().collection("habitup");
-console.log(ref);
+
+  const ref = firebase.firestore().collection("habitup");
+  console.log(ref);
 
   return (
-
     <PaddingStyles>
-      <H3> <Username /> Todo List</H3>
+      <H3>
+        {" "}
+        <Username /> Todo List
+      </H3>
       <form onSubmit={handleSubmit}>
         <InputGroup>
           <FormControl
@@ -58,23 +58,45 @@ console.log(ref);
             id="todoTask"
             value={todoItem.todoTask}
             onChange={handleChange}
-
-
           />
-          <Button variant="outline-light" name="impactSign" id="impactSign" value="+" onClick={handleChange}>Positive</Button>
-          <Button variant="outline-light" name="impactSign" id="impactSign" value="-" onClick={handleChange}>Negativ</Button>
-          <Button variant="outline-light" name="impactSign" id="impactSign" value="=" onClick={handleChange}>Postive</Button>
-          <Button variant="outline-light" type="submit">Add</Button>
-
+          <Button
+            variant="outline-light"
+            name="impactSign"
+            id="impactSign"
+            value="+"
+            onClick={handleChange}>
+            Positive
+          </Button>
+          <Button
+            variant="outline-light"
+            name="impactSign"
+            id="impactSign"
+            value="-"
+            onClick={handleChange}>
+            Negative
+          </Button>
+          <Button
+            variant="outline-light"
+            name="impactSign"
+            id="impactSign"
+            value="="
+            onClick={handleChange}>
+            Neutral
+          </Button>
+          <Button variant="outline-light" type="submit">
+            Add
+          </Button>
         </InputGroup>
       </form>
-      {Records.map((props) =>
-        <TodoItem todoTask={props.todoTask} impactSign={props.impactSign} key={props.id} />
-      )}
+      {Records.map((props) => (
+        <TodoItem
+          todoTask={props.todoTask}
+          impactSign={props.impactSign}
+          key={props.id}
+        />
+      ))}
     </PaddingStyles>
-
-
   );
-}
+};
 
 export default Todo;
